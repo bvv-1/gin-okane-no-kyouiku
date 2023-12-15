d0dcd50eaf539be2b6c1884e47bc3d9bc261d7d3
bvv-1 <fhinnoigsr@gmail.com>
2023-12-16 01:05:26 +0900

    Update main.go

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:5173", "https://okane-no-kyouiku.onrender.com"},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders: []string{
 			"Origin",
 			"Content-Type",
