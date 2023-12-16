f2294fdbb5c683f176aac54cdc5d24f46275c77f
bvv-1 <fhinnoigsr@gmail.com>
2023-12-16 18:13:48 +0900

    fix: remove unnecessary endpoints

--- docs/docs.go
+++ docs/docs.go
@@ -28,44 +28,19 @@ const docTemplate = `{
                 "produces": [
                     "application/json"
                 ],
-                "tags": [
-                    "hello"
-                ],
                 "summary": "Hello Worldのエンドポイント",
                 "operationId": "helloWorld",
                 "responses": {
                     "200": {
                         "description": "OK",
                         "schema": {
-                            "$ref": "#/definitions/httputil.HTTPError"
+                            "$ref": "#/definitions/httputil.SuccessResponse"
                         }
                     }
                 }
             }
         },
         "/api/v1/goals": {
-            "get": {
-                "description": "ユーザーの現在の目標を確認する",
-                "consumes": [
-                    "application/json"
-                ],
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "goals"
-                ],
-                "summary": "現在の目標を確認するエンドポイント",
-                "operationId": "checkGoal",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/main.GoalResponse"
-                        }
-                    }
-                }
-            },
             "post": {
                 "description": "Set a goal with associated tasks",
                 "consumes": [
@@ -160,118 +135,6 @@ const docTemplate = `{
                 }
             }
         },
-        "/api/v1/plans/accept": {
-            "post": {
-                "description": "ユーザーが提案されたデイリープランを受け入れる",
-                "consumes": [
-                    "application/json"
-                ],
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "plans"
-                ],
-                "summary": "提案されたデイリープランを受け入れるエンドポイント",
-                "operationId": "acceptSuggestedPlans",
-                "parameters": [
-                    {
-                        "description": "受け入れリクエストのボディ",
-                        "name": "request",
-                        "in": "body",
-                        "required": true,
-                        "schema": {
-                            "$ref": "#/definitions/main.AcceptRequest"
-                        }
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/main.OkResponse"
-                        }
-                    },
-                    "400": {
-                        "description": "Bad Request",
-                        "schema": {
-                            "$ref": "#/definitions/httputil.HTTPError"
-                        }
-                    }
-                }
-            }
-        },
-        "/api/v1/plans/check": {
-            "get": {
-                "description": "ユーザーのデイリープランが順調かどうかを確認する",
-                "consumes": [
-                    "application/json"
-                ],
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "plans"
-                ],
-                "summary": "デイリープランが順調かどうかを確認するエンドポイント",
-                "operationId": "checkProgress",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/main.AdjustmentResponse"
-                        }
-                    }
-                }
-            }
-        },
-        "/api/v1/plans/submit": {
-            "post": {
-                "description": "ユーザーがデイリータスクデータを提出する",
-                "consumes": [
-                    "application/json"
-                ],
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "submit"
-                ],
-                "summary": "デイリータスクデータを提出するエンドポイント",
-                "operationId": "submitDailyTasks",
-                "parameters": [
-                    {
-                        "description": "提出リクエストのボディ",
-                        "name": "request",
-                        "in": "body",
-                        "required": true,
-                        "schema": {
-                            "$ref": "#/definitions/main.SubmitRequest"
-                        }
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/main.OkResponse"
-                        }
-                    },
-                    "400": {
-                        "description": "Bad Request",
-                        "schema": {
-                            "$ref": "#/definitions/httputil.HTTPError"
-                        }
-                    },
-                    "500": {
-                        "description": "Internal Server Error",
-                        "schema": {
-                            "$ref": "#/definitions/httputil.HTTPError"
-                        }
-                    }
-                }
-            }
-        },
         "/api/v1/plans/suggested": {
             "get": {
                 "description": "Get a list of suggested plans",
@@ -313,71 +176,6 @@ const docTemplate = `{
                         "description": "OK",
                         "schema": {
                             "type": "string"
-                        }
-                    }
-                }
-            }
-        },
-        "/api/v1/plans/today": {
-            "post": {
-                "description": "ユーザーが指定した日のデイリープランを取得する",
-                "consumes": [
-                    "application/json"
-                ],
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "plans"
-                ],
-                "summary": "指定された日のデイリープランを取得するエンドポイント",
-                "operationId": "getDailyPlansOld",
-                "parameters": [
-                    {
-                        "description": "取得する日の番号",
-                        "name": "day",
-                        "in": "body",
-                        "required": true,
-                        "schema": {
-                            "$ref": "#/definitions/main.GetDailyPlansRequest"
-                        }
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/models.DailyPlansResponse"
-                        }
-                    },
-                    "400": {
-                        "description": "Bad Request",
-                        "schema": {
-                            "$ref": "#/definitions/httputil.HTTPError"
-                        }
-                    }
-                }
-            }
-        },
-        "/api/v1/points": {
-            "get": {
-                "description": "ユーザーの現在のポイントを取得する",
-                "consumes": [
-                    "application/json"
-                ],
-                "produces": [
-                    "application/json"
-                ],
-                "tags": [
-                    "points"
-                ],
-                "summary": "ユーザーのポイントを取得するエンドポイント",
-                "operationId": "getUserPoints",
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/main.PointsResponse"
                         }
                     }
                 }
@@ -630,88 +428,11 @@ const docTemplate = `{
                 }
             }
         },
-        "main.AcceptRequest": {
-            "type": "object",
-            "properties": {
-                "plans_ids_id": {
-                    "type": "integer"
-                },
-                "tasks_ids_id": {
-                    "type": "integer"
-                }
-            }
-        },
-        "main.AdjustmentResponse": {
-            "type": "object",
-            "properties": {
-                "adjusted_plans": {
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/models.SuggestedPlan"
-                    }
-                },
-                "message": {
-                    "type": "string"
-                }
-            }
-        },
-        "main.GetDailyPlansRequest": {
-            "type": "object",
-            "properties": {
-                "day": {
-                    "type": "integer"
-                }
-            }
-        },
-        "main.GoalResponse": {
-            "type": "object",
-            "properties": {
-                "goal": {
-                    "type": "string"
-                },
-                "goal_points": {
-                    "type": "integer"
-                }
-            }
-        },
-        "main.OkResponse": {
+        "httputil.SuccessResponse": {
             "type": "object",
             "properties": {
                 "message": {
                     "type": "string"
-                }
-            }
-        },
-        "main.PointsResponse": {
-            "type": "object",
-            "properties": {
-                "points": {
-                    "type": "integer"
-                }
-            }
-        },
-        "main.SubmitRequest": {
-            "type": "object",
-            "properties": {
-                "day": {
-                    "type": "integer"
-                },
-                "total_points": {
-                    "type": "integer"
-                }
-            }
-        },
-        "models.DailyPlansResponse": {
-            "type": "object",
-            "properties": {
-                "day": {
-                    "type": "integer"
-                },
-                "plans_today": {
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/models.Task"
-                    }
                 }
             }
         },
--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 
 	"time"
 
-	"github.com/gin-contrib/cors"
+	httputil "gin-okane-no-kyouiku/utils"
 
-	"golang.org/x/xerrors"
+	"github.com/gin-contrib/cors"
 
 	_ "gin-okane-no-kyouiku/docs"
 
@@ -16,7 +16,6 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 
 	"gin-okane-no-kyouiku/controllers"
-	"gin-okane-no-kyouiku/models"
 )
 
 // @title okane no kyouiku API
@@ -60,15 +59,6 @@ func main() {
 	r.GET("/api/v2/plans/today", controllers.GetTodayPlan)
 	r.POST("/api/v2/plans/today", controllers.SubmitTodayProgress)
 
-	r.POST("/api/v2/plans/suggest", controllers.SuggestDailyPlans) // 動詞を入れない
-	r.POST("/api/v1/plans/accept", acceptSuggestedPlans)           // 動詞を入れない
-	r.GET("/api/v1/goals", checkGoal)
-	r.GET("/api/v1/plans/check", checkProgress) // 動詞を入れない
-	r.POST("/api/v1/plans/today", getDailyPlansOld)
-	// r.GET("/api/v2/plans/today", controllers.GetTodayPlans)
-	r.POST("/api/v1/plans/submit", submitDailyTasks) // 動詞を入れない, せめて名詞
-	r.GET("/api/v1/points", getUserPoints)
-
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.Run(":8080")
@@ -78,197 +68,11 @@ func main() {
 // @Summary Hello Worldのエンドポイント
 // @Description GETリクエストに対して{"message": "Hello, World!"}を返す
 // @ID helloWorld
-// @Tags hello
 // @Accept json
 // @Produce json
-// @Success 200 {object} httputil.HTTPError
+// @Success 200 {object} httputil.SuccessResponse
 // @Router / [get]
 func helloWorld(c *gin.Context) {
-	data := map[string]string{"message": "Hello, World!"}
-	c.JSON(http.StatusOK, data)
-}
-
-type AcceptRequest struct {
-	PlanID int `json:"plans_ids_id"`
-	TaskID int `json:"tasks_ids_id"`
-}
-
-type OkResponse struct {
-	Message string `json:"message"`
-}
-
-type HTTPError struct {
-	Code         int
-	WrappedError error
-}
-
-// AcceptSuggestedPlans godoc
-// @Summary 提案されたデイリープランを受け入れるエンドポイント
-// @Description ユーザーが提案されたデイリープランを受け入れる
-// @ID acceptSuggestedPlans
-// @Tags plans
-// @Accept json
-// @Produce json
-// @Param request body AcceptRequest true "受け入れリクエストのボディ"
-// @Success 200 {object} OkResponse
-// @Failure 400 {object} httputil.HTTPError
-// @Router /api/v1/plans/accept [post]
-func acceptSuggestedPlans(c *gin.Context) {
-	var request AcceptRequest
-
-	if err := c.ShouldBindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": xerrors.Errorf("Invalid data format: %w", err).Error()})
-		return
-	}
-
-	// モックデータを使用してレスポンスを生成
-	response := OkResponse{
-		Message: "Plan accepted",
-	}
-
-	c.JSON(http.StatusOK, response)
-}
-
-type GoalResponse struct {
-	Goal       string `json:"goal"`
-	GoalPoints int    `json:"goal_points"`
-}
-
-// @Summary 現在の目標を確認するエンドポイント
-// @Description ユーザーの現在の目標を確認する
-// @ID checkGoal
-// @Tags goals
-// @Accept json
-// @Produce json
-// @Success 200 {object} GoalResponse
-// @Router /api/v1/goals [get]
-func checkGoal(c *gin.Context) {
-	// モックデータを使用してレスポンスを生成
-	response := GoalResponse{
-		Goal:       "computer",
-		GoalPoints: 100,
-	}
-
-	c.JSON(http.StatusOK, response)
-}
-func getAdjustedPlans() []models.SuggestedPlan {
-	return []models.SuggestedPlan{
-		{Day: 1, PlansToday: []models.Task{{Name: "cleaning", Point: 5}}},
-		// 他の日のプランも同様に追加
-	}
-}
-
-type AdjustmentResponse struct {
-	Message       string                 `json:"message"`
-	AdjustedPlans []models.SuggestedPlan `json:"adjusted_plans"`
-}
-
-// @Summary デイリープランが順調かどうかを確認するエンドポイント
-// @Description ユーザーのデイリープランが順調かどうかを確認する
-// @ID checkProgress
-// @Tags plans
-// @Accept json
-// @Produce json
-// @Success 200 {object} OkResponse
-// @Success 200 {object} AdjustmentResponse
-// @Router /api/v1/plans/check [get]
-func checkProgress(c *gin.Context) {
-	// モックデータを使用してレスポンスを生成
-	isOnTrack := true
-
-	if isOnTrack {
-		// デイリープランが順調な場合のレスポンス
-		okResponse := OkResponse{
-			Message: "Plans are on track",
-		}
-		c.JSON(http.StatusOK, okResponse)
-	} else {
-		// デイリープランが調整が必要な場合のレスポンス
-		adjustmentResponse := AdjustmentResponse{
-			Message:       "Plans need adjustment",
-			AdjustedPlans: getAdjustedPlans(),
-		}
-		c.JSON(http.StatusOK, adjustmentResponse)
-	}
-}
-
-type GetDailyPlansRequest struct {
-	Day int `json:"day"`
-}
-
-// @Summary 指定された日のデイリープランを取得するエンドポイント
-// @Description ユーザーが指定した日のデイリープランを取得する
-// @ID getDailyPlansOld
-// @Tags plans
-// @Accept json
-// @Produce json
-// @Param day body GetDailyPlansRequest true "取得する日の番号"
-// @Success 200 {object} models.DailyPlansResponse
-// @Failure 400 {object} httputil.HTTPError
-// @Router /api/v1/plans/today [post]
-func getDailyPlansOld(c *gin.Context) {
-	var request GetDailyPlansRequest
-
-	if err := c.ShouldBindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, xerrors.Errorf("Invalid data format: %w", err).Error())
-		return
-	}
-
-	// モックデータを使用してレスポンスを生成
-	response := models.DailyPlansResponse{
-		Day:        request.Day,
-		PlansToday: []models.Task{{Name: "cleaning", Point: 5}},
-	}
-
-	c.JSON(http.StatusOK, response)
-}
-
-type SubmitRequest struct {
-	Day         int `json:"day"`
-	TotalPoints int `json:"total_points"`
-}
-
-// @Summary デイリータスクデータを提出するエンドポイント
-// @Description ユーザーがデイリータスクデータを提出する
-// @ID submitDailyTasks
-// @Tags submit
-// @Accept json
-// @Produce json
-// @Param request body SubmitRequest true "提出リクエストのボディ"
-// @Success 200 {object} OkResponse
-// @Failure 400 {object} httputil.HTTPError
-// @Failure 500 {object} httputil.HTTPError
-// @Router /api/v1/plans/submit [post]
-func submitDailyTasks(c *gin.Context) {
-	var request SubmitRequest
-
-	if err := c.ShouldBindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, xerrors.Errorf("Invalid data format: %w", err).Error())
-		return
-	}
-
-	// モックデータを使用してレスポンスを生成
-	response := OkResponse{"Data received successfully"}
-	c.JSON(http.StatusOK, response)
-}
-
-type PointsResponse struct {
-	Points int `json:"points"`
-}
-
-// @Summary ユーザーのポイントを取得するエンドポイント
-// @Description ユーザーの現在のポイントを取得する
-// @ID getUserPoints
-// @Tags points
-// @Accept json
-// @Produce json
-// @Success 200 {object} PointsResponse
-// @Router /api/v1/points [get]
-func getUserPoints(c *gin.Context) {
-	// モックデータを使用してレスポンスを生成
-	response := PointsResponse{
-		Points: 88,
-	}
-
+	response := httputil.SuccessResponse{Message: "Hello, World!"}
 	c.JSON(http.StatusOK, response)
 }
